269086bfb764c38cc61bb2f2d8e41a3bb5b2cd43
mohaphez <mohaphez@yahoo.com>
2022-11-27 01:43:43 +0330

    init commit

---
+++ backend/src/Controller/ConvertController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use ICU;
+use Salarmehr\Cosmopolitan\Cosmo;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ConvertController extends AbstractController
+{
+
+    private $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    //  Return languages list. 
+    #[Route('/api/languages', name: 'app_language_list', methods: 'GET')]
+    function languageList(): JsonResponse
+    {
+        $languages = ICU::get_languages();
+
+        return $this->json([
+            'message' => 'Languages fetched successfully!',
+            'data' => $languages
+        ]);
+    }
+
+    // Spellout numbers based on language. 
+    #[Route('/api/convert', name: 'app_spellout_convert', methods: ['POST', 'OPTIONS'])]
+    function spellout(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $validationResult = $this->validateRequest($data);
+
+        if (count($validationResult) > 0) {
+            return $this->json([
+                'message' => 'We have some error',
+                'data' => $validationResult
+            ], 400);
+        }
+
+        $result = Cosmo::create($data['language'])->spellout(floatval($data['number']));
+
+        return $this->json([
+            'message' => 'Data Converted successfully!',
+            'data' => ["spellout" => $result]
+        ]);
+    }
+
+    // Validate convert body json fields
+    function validateRequest($data): array
+    {
+        $messages = [];
+        $constraints = new Assert\Collection([
+            'language' => [
+                new Assert\NotBlank(),
+            ],
+            'number' => [
+                new Assert\NotBlank()
+            ],
+        ]);
+        $errors = $this->validator->validate($data, $constraints);
+        foreach ($errors as $error) {
+            array_push($messages, $error->message);
+        }
+        return $messages;
+    }
+}
