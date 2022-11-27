d3895653e49f7ab7c16ea6ae81a03982819c7b6e
mohaphez <mohaphez@yahoo.com>
2022-11-27 11:30:57 +0330

    update docker-compose

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
-
+error_reporting(E_ALL ^ (E_NOTICE | E_WARNING | E_DEPRECATED));
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
